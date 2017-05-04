f0b6789187eeb09ea952b5bb24d870055bb1b26a
Pablo Cordero <dimenwarper@gmail.com>
2017-05-04 14:56:08 -0700

    Fixed mknod --> touch to avoid mac os x errors

--- weasl/utils.py
+++ weasl/utils.py
@@ -1,5 +1,9 @@
 import os
 
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
 def create_dir_structure(dir_structure):
     for branch in dir_structure:
         rest, node = os.path.split(branch)
@@ -7,6 +11,6 @@ def create_dir_structure(dir_structure):
             os.makedirs(rest)
         if not os.path.exists(branch):
             if '.' in node:
-                os.mknod(branch)
+                touch(branch)
             else:
                 os.mkdir(branch)
