8929d63c6af41702cfa2731a8184dada7aeae252
Nicholas Pataki <njpataki@gmail.com>
2017-05-04 14:24:28 -0700

    first commit for train command

--- weasl/commands.py
+++ weasl/commands.py
@@ -1,5 +1,6 @@
 import os
 
+from . import config
 from . import utils
 
 class Command(object):
@@ -47,3 +48,55 @@ class StartClassifier(Command):
 
         dirs = ['config']
         utils.create_dir_structure([os.path.join(dir, '%s.yaml' % clargs.name) for dir in dirs])
+
+class Train(Command):
+
+    def setup_clparser(self, parser):
+        parser.add_argument(
+            'name', 
+            type=str, 
+            help='Name of the classifier')
+        parser.add_argument(
+            '--train-file', 
+            type=str, 
+            help='Absolute path to training permits')
+        return parser
+
+    def set_up(self, clargs):
+
+        # construct path to feature_functions
+        # use eval(feature_func_string) 
+        # execture feature_func to get data
+        # hstack the results of these feature functions
+
+        # construct path to clfr
+        # use eval(clf_str)
+        # pass the featurized train_matrix
+
+        # access user-defined rules in rules.py
+        # apply these rules and create labels using pre-defined labeling module
+
+        # fit with rule_based_labels, train_matrix
+
+        # return trained_clf
+
+        config_dict = config.read_config(clargs.name)
+        featurizer = config_dict['featurizers'] 
+        clf = config_dict['classifier']
+
+
+        
+
+    def execute(self, clargs):
+
+        set_up()
+        fit()
+        predict()
+
+
+    def fit(self):
+        
+        raise NotImplementedError()
+
+    def predict(self):
+        raise NotImplementedError()
