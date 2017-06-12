578be396371020571d87fa5372f5d3c914eed331
Nicholas Pataki <njpataki@gmail.com>
2017-06-12 12:16:14 -0700

    use imp instead of importlib for import_module. In python 2.7 importlib does not accept full path to package

--- weasl/project_env.py
+++ weasl/project_env.py
@@ -1,21 +1,23 @@
-import importlib
+# import importlib
 import inspect
+import imp
 
-def _get_module(classifier_name, dir):
-    module_name = '%s.%s' % (dir, classifier_name)
-    return importlib.import_module(module_name)
+def _get_module(module_name, dir):
+    path = '%s/%s.py' % (dir, module_name)
+    return imp.load_source(module_name, path)
+
+def get_functions(module_name, dir):
+    module = _get_module(module_name, dir)
+    names_functions = inspect.getmembers(module, inspect.isfunction)
+    print names_functions
+    return dict(names_functions)
 
 def get_class(classifier_name, dir, class_name):
     module = _get_module(classifier_name, dir)
     class_ = getattr(module, class_name)
     return class_
 
-def get_function(classifier_name, dir, func_name):
-    module = _get_module(classifier_name, dir)
-    func_ = getattr(module, func_name)
-    return func_
-
-def get_functions(classifier_name, dir):
-    module = _get_module(classifier_name, dir)
-    names_functions = inspect.getmembers(module, inspect.isfunction)
-    return dict(names_functions)
+# def get_function(classifier_name, dir, func_name):
+#     module = _get_module(classifier_name, dir)
+#     func_ = getattr(module, func_name)
+#     return func_
