d645689dbdbc774e1f6d5d7f5ac2b221512400fa
Nicholas Pataki <njpataki@gmail.com>
2017-06-12 12:17:32 -0700

    updates to weasl's train command

--- weasl/commands.py
+++ weasl/commands.py
@@ -1,8 +1,12 @@
 import os
 
+import pandas as pd
+
+from . import project_env
 from . import features
 from . import config
 from . import utils
+
 
 class Command(object):
 
@@ -11,6 +15,7 @@ class Command(object):
 
     def execute(self, clargs):
         raise NotImplementedError('Command.execute not implemented!')
+
 
 class StartProjectCommand(Command):
 
@@ -33,6 +38,7 @@ class StartProjectCommand(Command):
         utils.create_dir_structure([os.path.join(clargs.name, dir) for dir in dirs])
         utils.touch(os.path.join(clargs.name, 'config', 'master.yaml'))
 
+
 class StartClassifier(Command):
 
     def setup_clparser(self, parser):
@@ -49,6 +55,7 @@ class StartClassifier(Command):
         dirs = ['config']
         utils.create_dir_structure([os.path.join(dir, '%s.yaml' % clargs.name) for dir in dirs])
 
+
 class Train(Command):
 
     def setup_clparser(self, parser):
@@ -62,40 +69,49 @@ class Train(Command):
             help='Absolute path to training permits')
         return parser
 
-    # use execute method to execute all the private methods 
     def _get_feature_functions(self, clargs):
-
-        # construct path to feature_functions
-        dirs = [os.path.join('featurizers', 'master.py'),
-                os.path.join('featurizers', clargs.name)]
+        dirs_module = [('featurizers', 'master'), ('featurizers', clargs.name)]
         names_features = {}
-        for dir in dirs:
-            names_features.update(project_env.get_functions(clargs.name,dir))
-        # use project_env.get_functions to get a list of feature functions available
-        # use features module to get a feature matrix
+        for dir, module in dirs_module:
+            names_features.update(project_env.get_functions(module, dir))
+        return names_features.values()
 
-        config_dict = config.read_config(clargs.name)
-        featurizer = config_dict['featurizers'] 
-        clf = config_dict['classifier']
+    def _get_rule_functions(self, clargs):
+        module, dir = clargs.name, 'rules'
+        names_rules = {}
+        names_rules.update(project_env.get_functions(module, dir))
+        return names_rules.values()
 
-    def _build_clf():
+    def _build_clf(self, clargs):
         # construct path to clfr
         # use project_env.get_class(path_to_clf_str)
         # instantiate the clf
         pass
 
-    def _fit():
+    def _fit(self, training_labels):
         # for each set of labels return a fitted classifier in a list
+        # requires a .fit on classifier (sklearn)
         pass
         
-    def _label_samples():
+    def _label_samples(self):
         # use labels.py to return a df of labels
         pass
 
-    def _serialize():
+    def _serialize(self):
         # serialize all the clfrs to disc using conventions in google sheet
         pass
 
     def execute(self, clargs):
-        pass
+        train_df = pd.read_csv(clargs.train_file)
+        feature_funcs = self._get_feature_functions(clargs)
+        train_matrix_df = features.call_and_concat(train_df, feature_funcs)
+        rule_funcs = self._get_rule_functions(clargs)
+        print rule_funcs
+        labels = [rule_func(train_df) for rule_func in rule_funcs]
+        print len(labels)
+        print labels[0][:10]
+        # for y_train in y_trains:
+        #     clf = self._build_clf(clargs)
+
+        
 
