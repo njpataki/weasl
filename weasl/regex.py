5291701239b32b739f7110689684b7f343ec885e
Nicholas Pataki <njpataki@gmail.com>
2017-06-12 12:16:38 -0700

    fix test of instance type

--- weasl/regex.py
+++ weasl/regex.py
@@ -9,8 +9,8 @@ def _match_regex(rgx, text):
     return match is not None
 
 def regex_search(rgx, text):
-    if isinstance(text) == str:
+    if isinstance(text, str):
         return _match_regex(rgx, text)
     else:
         np.array([_match_regex(rgx, text_item) for text_item in text])
-    
+        
