1ef2963be357b61ab1f861dbe46f8ce9dbfba5ed
Nicholas Pataki <njpataki@gmail.com>
2017-06-13 15:27:14 -0700

    add current working dir to sys.path to find module
    - python adds script's directory but not the current directory for
      mac 10.12.4, so this may be an os issue

--- weasl/project_env.py
+++ weasl/project_env.py
@@ -1,19 +1,21 @@
 import importlib
 import inspect
+import sys
+import os
 
 def _get_module(module_name, dir):
+    sys.path.append(os.getcwd())
     return importlib.import_module('%s.%s' % (dir, module_name))
 
 def get_functions(module_name, dir):
     module = _get_module(module_name, dir)
     names_functions = inspect.getmembers(module, inspect.isfunction)
-    print names_functions
     return dict(names_functions)
 
-def get_class(classifier_name, dir, class_name):
-    module = _get_module(classifier_name, dir)
-    class_ = getattr(module, class_name)
-    return class_
+# def get_class(classifier_name, dir, class_name):
+#     module = _get_module(classifier_name, dir)
+#     class_ = getattr(module, class_name)
+#     return class_
 
 # def get_function(classifier_name, dir, func_name):
 #     module = _get_module(classifier_name, dir)
